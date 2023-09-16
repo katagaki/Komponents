c2db83f83727d4c487d77b1135646a7cccb9f50d
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-16 12:56:04 +0800

    Delete tests

--- Package.swift
+++ Package.swift
@@ -18,9 +18,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Komponents"),
-        .testTarget(
-            name: "KomponentsTests",
-            dependencies: ["Komponents"]),
+            name: "Komponents")
     ]
 )
