9a19f583aadc19cc1e66b4a1c761ac0a14e59ca6
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-30 21:52:13 +0800

    Add close button view

---
+++ Sources/Komponents/CloseButton.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by シン・ジャスティン on 2023/09/30.
+//
+
+import SwiftUI
+
+public struct CloseButton: View {
+
+    public var action: () -> Void
+
+    public init(action: @escaping () -> Void) {
+        self.action = action
+    }
+
+    public var body: some View {
+        Button {
+            action()
+        } label: {
+            Image(systemName: "xmark.circle.fill")
+                .foregroundStyle(.primary)
+                .symbolRenderingMode(.hierarchical)
+                .font(.title2)
+        }
+        .buttonStyle(.plain)
+    }
+}
