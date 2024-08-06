4d173591b6c03c3a16cffc2aa9bdcfa3112f460f
katagaki <68231035+katagaki@users.noreply.github.com>
2024-08-06 21:03:34 +0900

    Add BarAccessoryButton component

---
+++ Sources/Komponents/BarAccessoryButton.swift
@@ -0,0 +1,73 @@
+//
+//  BarAccessoryButton.swift
+//  Komponents
+//
+//  Created by シン・ジャスティン on 2024/08/06.
+//
+
+import SwiftUI
+
+public struct BarAccessoryButton: View {
+
+    public var icon: String
+    public var text: LocalizedStringKey
+    public var action: () -> Void
+    public var isSecondary: Bool = false
+    public var isTextLight: Bool = true
+
+    public init(_ text: LocalizedStringKey, icon: String, isTextLight: Bool, action: @escaping () -> Void) {
+        self.icon = icon
+        self.text = text
+        self.isTextLight = isTextLight
+        self.action = action
+    }
+
+    public init(_ text: LocalizedStringKey, icon: String, isSecondary: Bool, action: @escaping () -> Void) {
+        self.icon = icon
+        self.text = text
+        self.action = action
+        self.isSecondary = isSecondary
+    }
+
+    public var body: some View {
+        Group {
+            if !isSecondary {
+                Button {
+                    action()
+                } label: {
+                    HStack(spacing: 8.0) {
+                        Image(systemName: icon)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18.0, height: 18.0)
+                        Text(text)
+                            .fontWeight(.medium)
+                    }
+                    .foregroundStyle((isTextLight ? Color.white : Color.black))
+                    .padding([.top, .bottom], 12.0)
+                    .padding([.leading, .trailing], 16.0)
+                    .background(Color.accentColor)
+                }
+            } else {
+                Button {
+                    action()
+                } label: {
+                    HStack(spacing: 8.0) {
+                        Image(systemName: icon)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18.0, height: 18.0)
+                        Text(text)
+                            .fontWeight(.medium)
+                    }
+                    .foregroundStyle(.primary)
+                    .padding([.top, .bottom], 12.0)
+                    .padding([.leading, .trailing], 16.0)
+                    .background(.primary.opacity(0.1))
+                }
+            }
+        }
+        .buttonStyle(.plain)
+        .clipShape(.capsule)
+    }
+}
