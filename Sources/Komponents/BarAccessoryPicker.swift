5fcb28393ad44ae94abd4dbf2466bd5b57c44d28
katagaki <68231035+katagaki@users.noreply.github.com>
2024-08-16 13:07:47 +0900

    Implement BarAccessoryPicker

---
+++ Sources/Komponents/BarAccessoryPicker.swift
@@ -0,0 +1,46 @@
+//
+//  BarAccessoryPicker.swift
+//  Komponents
+//
+//  Created by シン・ジャスティン on 2024/08/16.
+//
+
+import SwiftUI
+
+public struct BarAccessoryPicker<Content: View>: View {
+
+    @ViewBuilder let pickerItems: Content
+    public var icon: String?
+    public var text: LocalizedStringKey
+    public var selection: Binding<AnyHashable>
+
+    public init(_ text: LocalizedStringKey, icon: String? = nil, selection: Binding<AnyHashable>, @ViewBuilder pickerItems: @escaping () -> Content) {
+        self.pickerItems = pickerItems()
+        self.icon = icon
+        self.text = text
+        self.selection = selection
+    }
+
+    public var body: some View {
+        Picker(selection: selection) {
+            pickerItems
+        } label: {
+            HStack(spacing: 8.0) {
+                if let icon {
+                    Image(systemName: icon)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 18.0, height: 18.0)
+                }
+                Text(text)
+                    .fontWeight(.medium)
+            }
+            .foregroundStyle(.white)
+            .padding([.top, .bottom], 12.0)
+            .padding([.leading, .trailing], 16.0)
+            .background(Color.accentColor)
+        }
+        .buttonStyle(.plain)
+        .clipShape(.capsule)
+    }
+}
