ad96480d5782de608e16e4838b0747903d1b6ea7
katagaki <68231035+katagaki@users.noreply.github.com>
2024-10-19 21:47:17 +0900

    Change how app icon is fetched

--- Sources/Komponents/ListAppIconRow.swift
+++ Sources/Komponents/ListAppIconRow.swift
@@ -16,19 +16,17 @@ public struct ListAppIconRow: View {
 
     public var body: some View {
         HStack(alignment: .center, spacing: 16.0) {
-            if let apIconImage = UIImage(named: icon.imageName ?? "AppIcon") {
-                Image(uiImage: apIconImage)
-                    .resizable()
-                    .frame(width: 60.0, height: 60.0)
-                    .clipped(antialiased: true)
-                    .mask {
-                        RoundedRectangle(cornerRadius: 14.0)
-                    }
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 14.0)
-                            .stroke(.thickMaterial, lineWidth: 1.0)
-                    }
-            }
+            Image(((icon.imageName ?? "AppIcon") + ".Preview"))
+                .resizable()
+                .frame(width: 60.0, height: 60.0)
+                .clipped(antialiased: true)
+                .mask {
+                    RoundedRectangle(cornerRadius: 14.0)
+                }
+                .overlay {
+                    RoundedRectangle(cornerRadius: 14.0)
+                        .stroke(.thickMaterial, lineWidth: 1.0)
+                }
             Text(NSLocalizedString(icon.name, comment: ""))
             Spacer()
         }
